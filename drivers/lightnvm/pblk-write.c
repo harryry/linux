77c7da2eab7af1436ce2be44c163d7d4b93e2e43
harryry <puppydhkdlem@naver.com>
2018-09-25 16:03:37 +0900

    sdf

--- drivers/lightnvm/pblk-write.c
+++ drivers/lightnvm/pblk-write.c
@@ -987,7 +987,7 @@ void start_snapshot(struct pblk *pblk) {
 				meta_list[i].lba = cpu_to_le64(lba);
 				lba_list[paddr] = cpu_to_le64(lba);
 
-				pblk_down_rq(pblk, &rqd->ppa_list, nr_secs, lun_bitmap);
+				pblk_down_rq(pblk, rqd->ppa_list, nr_secs, lun_bitmap);
 
 			}
 		}
